14796ebc32bf9fc67d3515b97634e15b4467738e
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-04-18 19:11:01 +0000

    fix appveyor file

---
+++ p5/addons/p5.sound.js
@@ -0,0 +1,17 @@
+/*! p5.sound.js v0.3.7 2018-01-19 */
+/**
+ *  p5.sound extends p5 with <a href="http://caniuse.com/audio-api"
+ *  target="_blank">Web Audio</a> functionality including audio input,
+ *  playback, analysis and synthesis.
+ *  <br/><br/>
+ *  <a href="#/p5.SoundFile"><b>p5.SoundFile</b></a>: Load and play sound files.<br/>
+ *  <a href="#/p5.Amplitude"><b>p5.Amplitude</b></a>: Get the current volume of a sound.<br/>
+ *  <a href="#/p5.AudioIn"><b>p5.AudioIn</b></a>: Get sound from an input source, typically
+ *    a computer microphone.<br/>
+ *  <a href="#/p5.FFT"><b>p5.FFT</b></a>: Analyze the frequency of sound. Returns
+ *    results from the frequency spectrum or time domain (waveform).<br/>
+ *  <a href="#/p5.Oscillator"><b>p5.Oscillator</b></a>: Generate Sine,
+ *    Triangle, Square and Sawtooth waveforms. Base class of
+ *    <a href="#/p5.Noise">p5.Noise</a> and <a href="#/p5.Pulse">p5.Pulse</a>.
+ *    <br/>
+ *  <a href="#/p5.Env"><b>p5.Env</b></a>: An Envelope is a ser
