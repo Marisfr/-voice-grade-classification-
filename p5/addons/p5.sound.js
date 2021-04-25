0706935de08bd65cd0632ccfdb3b2b348a4bf76e
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-04-25 19:45:28 +0000

    tg3: update rx_jumbo_pending ring param only when jumbo frames are enabled

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -80,4 +80,24 @@ sndcore = function () {
      Copyright 2013 Chris Wilson
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
-     
+     You may obtain a copy of the License at
+         http://www.apache.org/licenses/LICENSE-2.0
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+  */
+  (function () {
+    function fixSetTarget(param) {
+      if (!param)
+        // if NYI, just return
+        return;
+      if (!param.setTargetAtTime)
+        param.setTargetAtTime = param.setTargetValueAtTime;
+    }
+    if (window.hasOwnProperty('webkitAudioContext') && !window.hasOwnProperty('AudioContext')) {
+      window.AudioContext = window.webkitAudioContext;
+      if (typeof AudioContext.prototype.createGain !== 'function')
+        AudioContext.prototype.createGain = AudioContext.prototype.createGainNode;
+      if (typeof AudioC
