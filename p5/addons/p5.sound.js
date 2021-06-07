af124e9a3d767939336fd5c151883be678909c5b
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-06-07 20:54:00 +0000

    Updated TODO list.

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -1923,4 +1923,27 @@ soundfile = function () {
     var peaksArray = Object.keys(peaksObj).sort();
     for (var index = 0; index < peaksArray.length; index++) {
       // find intervals in comparison to nearby peaks
-      
+      for (var i = 0; i < 10; i++) {
+        var startPeak = peaksObj[peaksArray[index]];
+        var endPeak = peaksObj[peaksArray[index + i]];
+        if (startPeak && endPeak) {
+          var startPos = startPeak.sampleIndex;
+          var endPos = endPeak.sampleIndex;
+          var interval = endPos - startPos;
+          // add a sample interval to the startPeak in the allPeaks array
+          if (interval > 0) {
+            startPeak.intervals.push(interval);
+          }
+          // tally the intervals and return interval counts
+          var foundInterval = intervalCounts.some(function (intervalCount) {
+            if (intervalCount.interval === interval) {
+              intervalCount.count++;
+              return intervalCount;
+            }
+          });
+          // store with JSON like formatting
+          if (!foundInterval) {
+            intervalCounts.push({
+              interval: interval,
+              count: 1
+            
