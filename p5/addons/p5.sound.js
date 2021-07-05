b1997f0793452382011122671a2babc58fa94244
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-07-05 12:04:40 +0000

    More cleaning.

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -2478,4 +2478,36 @@ fft = function () {
    *    var spectrum = fft.analyze();
    *    noStroke();
    *    fill(0,255,0); // spectrum is green
-   *    for (var i = 0; i
+   *    for (var i = 0; i< spectrum.length; i++){
+   *      var x = map(i, 0, spectrum.length, 0, width);
+   *      var h = -height + map(spectrum[i], 0, 255, height, 0);
+   *      rect(x, height, width / spectrum.length, h )
+   *    }
+   *
+   *    var waveform = fft.waveform();
+   *    noFill();
+   *    beginShape();
+   *    stroke(255,0,0); // waveform is red
+   *    strokeWeight(1);
+   *    for (var i = 0; i< waveform.length; i++){
+   *      var x = map(i, 0, waveform.length, 0, width);
+   *      var y = map( waveform[i], -1, 1, 0, height);
+   *      vertex(x,y);
+   *    }
+   *    endShape();
+   *
+   *    text('click to play/pause', 4, 10);
+   *  }
+   *
+   *  // fade sound if mouse is over canvas
+   *  function togglePlay() {
+   *    if (sound.isPlaying()) {
+   *      sound.pause();
+   *    } else {
+   *      sound.loop();
+   *    }
+   *  }
+   *  </code></div>
+   */
+  p5.FFT = function (smoothing, bins) {
+    this.input = this.analyser = p5sound.audiocontext.createAnalyser()
