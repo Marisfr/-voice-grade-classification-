ca56eee8c290fa17fa6ecb1c73db14e6de537aba
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-07-13 09:06:34 +0000

    Updated Assamese translation

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -2826,4 +2826,36 @@ fft = function () {
      *
      *
      *function draw(){
-    
+     *
+     *  var centroidplot = 0.0;
+     *  var spectralCentroid = 0;
+     *
+     *
+     *  background(0);
+     *  stroke(0,255,0);
+     *  var spectrum = fft.analyze();
+     *  fill(0,255,0); // spectrum is green
+     *
+     *  //draw the spectrum
+     *
+     *  for (var i = 0; i< spectrum.length; i++){
+     *    var x = map(log(i), 0, log(spectrum.length), 0, width);
+     *    var h = map(spectrum[i], 0, 255, 0, height);
+     *    var rectangle_width = (log(i+1)-log(i))*(width/log(spectrum.length));
+     *    rect(x, height, rectangle_width, -h )
+     *  }
+
+     *  var nyquist = 22050;
+     *
+     *  // get the centroid
+     *  spectralCentroid = fft.getCentroid();
+     *
+     *  // the mean_freq_index calculation is for the display.
+     *  var mean_freq_index = spectralCentroid/(nyquist/spectrum.length);
+     *
+     *  centroidplot = map(log(mean_freq_index), 0, log(spectrum.length), 0, width);
+     *
+     *
+     *  stroke(255,0,0); // the line showing where the centroid is will be red
+     *
+ 
