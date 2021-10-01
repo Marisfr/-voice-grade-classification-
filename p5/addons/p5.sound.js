cdc7f20918ddb4de3fecc37ef1bed8e83366a983
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-10-01 05:44:56 +0000

    readme change

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -4648,4 +4648,35 @@ Tone_core_Param = function (Tone) {
   };
   return Tone.Param;
 }(Tone_core_Tone);
-/** Tone.js module by Y
+/** Tone.js module by Yotam Mann, MIT License 2016  http://opensource.org/licenses/MIT **/
+var Tone_core_Gain;
+Tone_core_Gain = function (Tone) {
+  'use strict';
+  if (window.GainNode && !AudioContext.prototype.createGain) {
+    AudioContext.prototype.createGain = AudioContext.prototype.createGainNode;
+  }
+  Tone.Gain = function () {
+    var options = this.optionsObject(arguments, [
+      'gain',
+      'units'
+    ], Tone.Gain.defaults);
+    this.input = this.output = this._gainNode = this.context.createGain();
+    this.gain = new Tone.Param({
+      'param': this._gainNode.gain,
+      'units': options.units,
+      'value': options.gain,
+      'convert': options.convert
+    });
+    this._readOnly('gain');
+  };
+  Tone.extend(Tone.Gain);
+  Tone.Gain.defaults = {
+    'gain': 1,
+    'convert': true
+  };
+  Tone.Gain.prototype.dispose = function () {
+    Tone.Param.prototype.dispose.call(this);
+    this._gainNode.disconnect();
+    this._gainNode = null;
+    this._writable('gain');
+    this.gai
