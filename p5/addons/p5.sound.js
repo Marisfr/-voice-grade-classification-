735f2ef2e5d42406de9f4f0fafeb03500ba348d6
Marisfr <93488933+Marisfr@users.noreply.github.com>
2021-10-16 14:59:12 +0000

    widget: Move some methods to VteTerminalPrivate

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -4990,4 +4990,38 @@ oscillator = function () {
    *  @constructor
    *  @param {Number} [freq] frequency defaults to 440Hz
    *  @param {String} [type] type of oscillator. Options:
-   *                         
+   *                         'sine' (default), 'triangle',
+   *                         'sawtooth', 'square'
+   *  @example
+   *  <div><code>
+   *  var osc;
+   *  var playing = false;
+   *
+   *  function setup() {
+   *    backgroundColor = color(255,0,255);
+   *    textAlign(CENTER);
+   *
+   *    osc = new p5.Oscillator();
+   *    osc.setType('sine');
+   *    osc.freq(240);
+   *    osc.amp(0);
+   *    osc.start();
+   *  }
+   *
+   *  function draw() {
+   *    background(backgroundColor)
+   *    text('click to play', width/2, height/2);
+   *  }
+   *
+   *  function mouseClicked() {
+   *    if (mouseX > 0 && mouseX < width && mouseY < height && mouseY > 0) {
+   *      if (!playing) {
+   *        // ramp amplitude to 0.5 over 0.05 seconds
+   *        osc.amp(0.5, 0.05);
+   *        playing = true;
+   *        backgroundColor = color(0,255,255);
+   *      } else {
+   *        // ramp amplitude to 0 over 0.5 seconds
+   *        osc.amp(0, 0.5);
+   *        playing = false;
+   *        back
