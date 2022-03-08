366733ffbcb98a840f123893a32a722cf9e2660d
Marisfr <93488933+Marisfr@users.noreply.github.com>
2022-03-08 18:03:09 +0000

    add normalize.css

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -8131,4 +8131,38 @@ filter = function () {
    *  function setup() {
    *    fill(255, 40, 255);
    *
-   *    filter = new 
+   *    filter = new p5.BandPass();
+   *
+   *    noise = new p5.Noise();
+   *    // disconnect unfiltered noise,
+   *    // and connect to filter
+   *    noise.disconnect();
+   *    noise.connect(filter);
+   *    noise.start();
+   *
+   *    fft = new p5.FFT();
+   *  }
+   *
+   *  function draw() {
+   *    background(30);
+   *
+   *    // set the BandPass frequency based on mouseX
+   *    var freq = map(mouseX, 0, width, 20, 10000);
+   *    filter.freq(freq);
+   *    // give the filter a narrow band (lower res = wider bandpass)
+   *    filter.res(50);
+   *
+   *    // draw filtered spectrum
+   *    var spectrum = fft.analyze();
+   *    noStroke();
+   *    for (var i = 0; i < spectrum.length; i++) {
+   *      var x = map(i, 0, spectrum.length, 0, width);
+   *      var h = -height + map(spectrum[i], 0, 255, height, 0);
+   *      rect(x, height, width/spectrum.length, h);
+   *    }
+   *
+   *    isMouseOverCanvas();
+   *  }
+   *
+   *  function isMouseOverCanvas() {
+   *    var mX 
