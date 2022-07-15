ff6932770e5926709437f287afe2b4c665090000
Marisfr <93488933+Marisfr@users.noreply.github.com>
2022-07-15 22:07:49 +0000

    ARM: dts: msm: update memory map for msm8976/8956/8952 for External release

--- p5/addons/p5.sound.js
+++ p5/addons/p5.sound.js
@@ -12225,4 +12225,27 @@ var distortion;
 distortion = function () {
   var Effect = effect;
   /*
-   * Adapted from [Kevin Ennis on StackOverflow](http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emu
+   * Adapted from [Kevin Ennis on StackOverflow](http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion)
+   */
+  function makeDistortionCurve(amount) {
+    var k = typeof amount === 'number' ? amount : 50;
+    var numSamples = 44100;
+    var curve = new Float32Array(numSamples);
+    var deg = Math.PI / 180;
+    var i = 0;
+    var x;
+    for (; i < numSamples; ++i) {
+      x = i * 2 / numSamples - 1;
+      curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+    }
+    return curve;
+  }
+  /**
+   * A Distortion effect created with a Waveshaper Node,
+   * with an approach adapted from
+   * [Kevin Ennis](http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion)
+   *
+   * This class extends <a href = "/reference/#/p5.Effect">p5.Effect</a>.
+   * Methods <a href = "/reference/#/p5.Effect/amp">amp()</a>, <a href = "/reference/#/p5.Effect/chain">chain()</a>,
+   * <a href = "/reference/#/p5.Effect/drywet">drywet()</a>, <a href = "/reference/#/p5.Effect/connect">connect()</a>, and
+   * <a href = "/reference/#/p5.Effect/disconnect">disconnect()</a> are av
