5fd40d84e7962151854d5d3a27bf8cae83c64fb0
Marisfr <93488933+Marisfr@users.noreply.github.com>
2023-02-23 07:13:29 +0000

    sw

--- sketch.js
+++ sketch.js
@@ -43,4 +43,46 @@ function draw() {
       symbol.y * pScale > height ||
       symbol.y * pScale < 0) {
     } else {
-      var index = (vide
+      var index = (video.width - Math.ceil(symbol.x) + 1 + (Math.ceil(symbol.y) * video.width)) * 4;
+
+      r = video.pixels[index];
+      g = video.pixels[index + 1];
+      b = video.pixels[index + 2];
+
+      brightness = (r + g + b) / 3;
+      gBrightness = map(g, 0, 255, 20, 260);
+
+      if (!r || ! g || ! b) {
+        fill(0, 0, 0);
+      } else {
+        fill(30, g, 30, gBrightness);
+      }
+      text(symbol.value, symbol.x * pScale, symbol.y * pScale);
+    }
+    symbol.rain();
+    symbol.setToRandomSymbol();
+  });
+
+}
+
+function Symbol(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.value;
+  this.speed = speed;
+
+  this.switchInterval = round(random(2, 25));
+
+  this.setToRandomSymbol = function() {
+    if (frameCount % this.switchInterval == 0) {
+        this.value = String.fromCharCode(
+          0x30A0 + round(random(0, 96))
+      );
+    }
+  }
+
+  this.rain = function() {
+    this.y = (this.y * pScale >= height) ? 0 : this.y += this.speed;
+  }
+
+}
